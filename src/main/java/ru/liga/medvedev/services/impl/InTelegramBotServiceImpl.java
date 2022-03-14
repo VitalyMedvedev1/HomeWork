d09be558c30ee69b9050d6030386796de9dd70a3
Vitaly Medvedev <vemedvedev@fil-it.ru>
2022-03-14 10:18:34 +0300

    HomeWork_2 telegram bot with only AVGAlg

---
+++ src/main/java/ru/liga/medvedev/services/impl/InTelegramBotServiceImpl.java
@@ -0,0 +1,21 @@
+package ru.liga.medvedev.services.impl;
+
+import org.springframework.stereotype.Component;
+import ru.liga.medvedev.domain.CommandLineParser;
+import ru.liga.medvedev.domain.Commands;
+import ru.liga.medvedev.domain.Reference;
+import ru.liga.medvedev.services.CommandService;
+
+@Component("BotService")
+public class InTelegramBotServiceImpl implements CommandService {
+
+    @Override
+    public Commands getCommands() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Commands getCommands(String text) {
+        return Reference.COMMAND_LINE_PARSER.parse(text);
+    }
+}

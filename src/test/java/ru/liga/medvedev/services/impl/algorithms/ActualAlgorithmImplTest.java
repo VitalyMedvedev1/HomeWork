c936400176787f0fdf4ba16acbf02cf1b266f1bc
Vitaly Medvedev <vemedvedev@fil-it.ru>
2022-03-18 22:30:06 +0300

    HomeWork_2 tests

--- src/test/java/ru/liga/medvedev/services/impl/algorithms/ActualAlgorithmImplTest.java
+++ src/test/java/ru/liga/medvedev/services/impl/algorithms/ActualAlgorithmImplTest.java
@@ -1,6 +1,6 @@
-/*
 package ru.liga.medvedev.services.impl.algorithms;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import ru.liga.medvedev.SpringConfiguration;
 import ru.liga.medvedev.domain.Command;
@@ -15,23 +15,22 @@ class ActualAlgorithmImplTest {
     List<Rate> listRate = new ArrayList<>();
     ActualAlgorithmImpl actualAlgorithm = new ActualAlgorithmImpl();
 
-*/
-/*    @Before
+    @Before
     public void addRateList() {
-        Commands commands = SpringConfiguration.COMMAND_CONTROLLER.getCommand("rate USD -period week -alg moon");
-        List<List<String>> list = fileSystemCsvRepository.getRateDataRepository(commands);
+        Command command = SpringConfiguration.COMMAND_CONTROLLER.getCommand("rate USD -period week -alg moon");
+        List<List<String>> list = SpringConfiguration.DATA_REPOSITORY_CONTROLLER.getRateDataRepository(command.getListCurrency().get(0));
         listRate = SpringConfiguration.ALGORITHMS_RATE_CONTROLLER.generateStatisticRateCurrency(
-                SpringConfiguration.RATE_DATA_MAPPER.mapRate(list), commands);
-    }*//*
+                SpringConfiguration.RATE_DATA_MAPPER.mapRate(list, command.getListCurrency().get(0)), command);
+    }
 
 
     @Test
     public void generateRate() {
         Command command = SpringConfiguration.COMMAND_CONTROLLER.getCommand("rate USD -Date 13.03.2022 -alg moon");
-        List<List<String>> list = SpringConfiguration.DATA_REPOSITORY_CONTROLLER.getRateDataRepository(command.getCurrency());
-        listRate = SpringConfiguration.RATE_DATA_MAPPER.mapRate(list, );
+        List<List<String>> list = SpringConfiguration.DATA_REPOSITORY_CONTROLLER.getRateDataRepository(command.getListCurrency().get(0));
+        listRate = SpringConfiguration.RATE_DATA_MAPPER.mapRate(list, command.getListCurrency().get(0));
         LocalDate localDate = command.getLocalDate();
         List<Rate> newRateStatistic = actualAlgorithm.getRateStatistic(localDate, listRate);
         System.out.println("123");
     }
-}*/
+}
